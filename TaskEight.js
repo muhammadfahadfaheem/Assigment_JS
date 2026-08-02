c3df7f130552eae327b2ea548cab5b84d68cc32a
muhammadfahadfaheem <fahadfahim945@gmail.com>
2026-08-02 13:52:55 +0500

    My Eight Task

---
+++ TaskEight.js
@@ -0,0 +1,11 @@
+let marks = 40;
+
+while (marks <=100) {
+
+    if (marks >= 50){
+        console.log(marks + "Pass");
+    } else{
+        console.log(marks + "Fail");
+    }
+    marks += 10;
+}

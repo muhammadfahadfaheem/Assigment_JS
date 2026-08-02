f2e8f523657d411099447c4238dc73de1fe04810
muhammadfahadfaheem <fahadfahim945@gmail.com>
2026-08-02 19:27:34 +0500

    my Nine Task

---
+++ TaskNine.js
@@ -0,0 +1,12 @@
+let i =1;
+ while(i<=10){
+    let result =2 * i;
+
+    if (result % 2 === 0){
+        console.log(result + "Even");
+    }else{
+        console.log(result + "Odd");
+    }
+
+    i++;
+ }

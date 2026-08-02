eb12ff1513a57e6498e08c40b668f1369435f9ae
muhammadfahadfaheem <fahadfahim945@gmail.com>
2026-08-02 13:05:59 +0500

    My Two Task js

---
+++ Task_two.js
@@ -0,0 +1,13 @@
+let i = -5;
+while(i <= 5){
+    if (i>0){
+        console.log (i + "=Positive");
+    } else if (i < 0) {
+        console.log(i + "=Negative");
+    }else {
+        console.log(i+"+Zero");
+ 
+    }
+i++;
+
+}
